14e75ed3079b31cff3045150de21437412ff6082
Dave Nice <32769325+davenice@users.noreply.github.com>
2026-06-05 23:21:00 +0100

    add icon

---
+++ scripts/generate-icons.mjs
@@ -0,0 +1,15 @@
+import sharp from 'sharp';
+import { readFileSync } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const svg = readFileSync(join(__dirname, '../public/icons/icon.svg'));
+
+for (const size of [192, 512]) {
+  await sharp(svg)
+    .resize(size, size)
+    .png()
+    .toFile(join(__dirname, `../public/icons/icon-${size}.png`));
+  console.log(`Generated icon-${size}.png`);
+}
